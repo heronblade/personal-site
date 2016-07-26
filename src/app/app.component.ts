54e49d4760c2af496eb7d044e9725f1b346a083d
heronblade <bylerj@gmail.com>
2016-07-25 22:10:01 -0400

    added home page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HomePageComponent]
 })
 
 export class AppComponent {
